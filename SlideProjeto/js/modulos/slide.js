812a7c2ba9a76b427baad2fe869a8d57c1e09321
EX-Stras <stras3pp@gmail.com>
2026-07-28 21:56:07 -0300

    agora o slide se move!

--- SlideProjeto/js/modulos/slide.js
+++ SlideProjeto/js/modulos/slide.js
@@ -2,26 +2,40 @@ export default class Slide {
   constructor(selector1, selector2) {
     this.selector1 = document.querySelector(selector1); // slide
     this.selector2 = document.querySelector(selector2); // wrapper
+    this.$ = { finalPosition: 0, startX: 0, movement: 0 };
   }
 
   addEvents() {
     this.selector2.addEventListener('mousedown', this.handleDown);
   }
 
+  moveSlide() {
+    this.selector1.setAttribute('style', `transform: translate3d(${(this.$.finalPosition + this.$.movement) * -1}px, 0, 0)`);
+  }
+
+  updatePosition(x) {
+    this.$.movement = (this.$.startX - x) * 1.6;
+  }
+
   handleUp() {
     this.selector1.removeEventListener('mousemove', this.handleMove);
     window.removeEventListener('mouseup', this.handleUp);
+
+    this.$.finalPosition += this.$.movement;
   }
 
-  handleMove(event) {
-    console.log(event.x);
+  handleMove({ x }) {
+    this.updatePosition(x);
+    this.moveSlide();
+
     return this;
   }
 
   handleDown(event) {
     event.preventDefault();
+    this.$.startX = event.x;
     this.selector1.addEventListener('mousemove', this.handleMove);
-    this.selector2.addEventListener('mouseup', this.handleUp);
+    window.addEventListener('mouseup', this.handleUp);
   }
 
   bindEvents() {
